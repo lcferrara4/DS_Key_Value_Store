3a1b83fdc8783101525e66ed2ca0229845560f67
Teddy Brombach <Teddy@teddys-mbp.dhcp.nd.edu>
2016-10-24 21:04:16 -0400

    done with code

--- frequencies.cpp
+++ frequencies.cpp
@@ -1,10 +1,96 @@
 // freqencies.cpp
 
 #include "map.h"
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <strings.h>
+#include <unistd.h>
+#include <chrono>
+#include <numeric>
 
 // Main execution --------------------------------------------------------------
+void usage(int status) {
+    std::cout << "usage: map_test" << std::endl
+              << "    -b BACKEND    Which Map backend (unsorted, sorted, bst, rbtree, treap)" << std::endl
+              << " dump DUMP_KEY, DUMP_VALUE, DUMP_KEY_VALUE, DUMP_VALUE_KEY" << std::endl;
+
+    exit(status);
+}
+
+std::string increment(const std::string &s){
+	int num = stoi(s);
+	num = num +1;
+	return std::to_string(num);
+
+}
+
+void parse_command_line_options(int argc, char *argv[], Map *&map) {
+    int c;
+
+    while ((c = getopt(argc, argv, "hb:")) != -1) {
+        switch (c) {
+            case 'b':
+                if (strcasecmp(optarg, "unsorted") == 0) {
+                    map = new UnsortedMap();
+                } else if (strcasecmp(optarg, "sorted") == 0) {
+                    map = new SortedMap();
+                } else if (strcasecmp(optarg, "bst") == 0) {
+                    map = new BSTMap();
+                } else if (strcasecmp(optarg, "rbtree") == 0) {
+                    map = new RBTreeMap();
+                } else if (strcasecmp(optarg, "treap") == 0) {
+                    map = new TreapMap();
+                } else {
+                    usage(1);
+                }
+                break;
+            case 'h':
+                usage(0);
+                break;
+            default:
+                usage(1);
+                break;
+        }
+    }
+
+    if (map == nullptr) {
+    	map = new UnsortedMap();
+    }
+}
 
 int main(int argc, char *argv[]) {
+	Map *map = nullptr;
+
+    if(argc !=4){
+		usage(1);
+	}
+    parse_command_line_options(argc, argv, map);
+
+    DumpFlag d;
+    
+    if (strcasecmp(argv[3], "DUMP_KEY") == 0 ){
+    	d = DUMP_KEY;
+   	} else if(strcasecmp(argv[3], "DUMP_VALUE") == 0 ){
+   		d = DUMP_VALUE;
+   	} else if(strcasecmp(argv[3], "DUMP_KEY_VALUE") == 0 ){
+   		d = DUMP_KEY_VALUE;
+   	} else if(strcasecmp(argv[3], "DUMP_VALUE_KEY") == 0 ){
+   		d = DUMP_VALUE_KEY;
+   	} else{
+   		usage(1);
+   	}
+
+    std::string word;
+    while(std::cin >> word){
+    	auto result = map->search(word);
+    	if(result == NONE){
+    		map->insert(word,"1");
+    	}else{
+    		map->insert(word,increment(result.second));
+    	}
+    }
+    map->dump(std::cout,d);
     return 0;
 }
 
--- map_bench.cpp
+++ map_bench.cpp
@@ -1,10 +1,103 @@
 // map_bench.cpp
 
 #include "map.h"
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <strings.h>
+#include <unistd.h>
+#include <chrono>
+#include <numeric>
 
 // Main execution --------------------------------------------------------------
+void usage(int status) {
+    std::cout << "usage: map_test" << std::endl
+              << "    -b BACKEND    Which Map backend (unsorted, sorted, bst, rbtree, treap)" << std::endl;
+
+    exit(status);
+}
+
+void parse_command_line_options(int argc, char *argv[], Map *&map) {
+    int c;
+
+    while ((c = getopt(argc, argv, "hb:")) != -1) {
+        switch (c) {
+            case 'b':
+                if (strcasecmp(optarg, "unsorted") == 0) {
+                    map = new UnsortedMap();
+                } else if (strcasecmp(optarg, "sorted") == 0) {
+                    map = new SortedMap();
+                } else if (strcasecmp(optarg, "bst") == 0) {
+                    map = new BSTMap();
+                } else if (strcasecmp(optarg, "rbtree") == 0) {
+                    map = new RBTreeMap();
+                } else if (strcasecmp(optarg, "treap") == 0) {
+                    map = new TreapMap();
+                } else {
+                    usage(1);
+                }
+                break;
+            case 'h':
+                usage(0);
+                break;
+            default:
+                usage(1);
+                break;
+        }
+    }
+
+    if (map == nullptr) {
+    	map = new UnsortedMap();
+    }
+}
+
+std::string int_to_key( int i, size_t padlength){
+	std::string s = std::to_string(i);
+	if( padlength > s.size() ){
+		s.insert(0, padlength - s.size(), '0');
+	}
+	return s;
+}
 
 int main(int argc, char *argv[]) {
+	Map *map = nullptr;
+
+	parse_command_line_options(argc, argv, map);
+
+	if(argc !=5){
+		usage(1);
+	}
+
+	int N = atoi(argv[3]);
+	size_t padlength = atoi(argv[4]);
+
+
+	auto start = std::chrono::high_resolution_clock::now();
+	
+	// Insert 1 - N
+    for (int i = 0; i < N; i++) {
+		std::string s = int_to_key(i, padlength); 
+    	map->insert(s, s);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> diff = end-start;
+    std::cout << "Time to insert: " << diff.count() << std::endl;
+
+	start = std::chrono::high_resolution_clock::now();
+    
+    // Search 1 - N
+    for (int i = 0; i < N; i++) {
+		std::string s = int_to_key(i, padlength); 
+		assert(map->search(s) != NONE);
+    }
+
+    end = std::chrono::high_resolution_clock::now();
+
+    diff = end-start;
+    std::cout << "Time to insert: " << diff.count() << std::endl;
+
     return 0;
 }
 
